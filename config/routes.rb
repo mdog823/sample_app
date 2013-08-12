67bf4a578e4dec902802a631525d5250c868e344
Matthew Wachter <wachter95@aol.com>
2013-08-12 11:42:55 -0400

    Add a fullt_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ SampleApp::Application.routes.draw do
 
   get "static_pages/about"
 
+  get "static_pages/contact"
+
   root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
